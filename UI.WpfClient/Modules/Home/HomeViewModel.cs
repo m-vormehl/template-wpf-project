2d94118544e76f6f096ccc978dd497ae027807d9
Michał Formella <michal.formella@gmail.com>
2019-07-05 01:59:25 +0200

    improved home page - made responsive-like

--- UI.WpfClient/Modules/Home/HomeViewModel.cs
+++ UI.WpfClient/Modules/Home/HomeViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Caliburn.Micro;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using UI.WpfClient.Interfaces;
@@ -10,17 +11,12 @@ namespace UI.WpfClient.Modules.Home
 {
     public class HomeViewModel : Screen, IDashBoard
     {
-        public IObservableCollection<object> DisplayedTasksList { get; set; }
-        public object RightSideContent { get; set; }
-
         private IEventAggregator _eventAggregator;
 
         public HomeViewModel(IEventAggregator eventAggregator)
         {
             _eventAggregator = eventAggregator;
             DisplayName = "Dashboard page";
-            //DisplayedTasksList = new IObservableCollection<string>();
-            RightSideContent = new HomeChart { DataContext= this};
         }    
         
         public void DashboardClick()
